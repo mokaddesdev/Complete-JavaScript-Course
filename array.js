96abbeb0d73471c6d70577004f7047185b741438
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2026-01-24 23:08:55 +0600

    delete 2 file commit

--- array.js
+++
@@ -1,9 +0,0 @@
-var name1 = "Ali1";
-var name2 ='Ali2';
-var name3 = "Ali3";
-var name4 = "Ali4";
-var name5 = "Ali4";
-var name6 = "Ali5";
-var name7 = "Ali6";
-
-console.log(name1,name2, name3, name4, name5)
