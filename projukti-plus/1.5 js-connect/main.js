45e4f1afb6bc84211933952c877e69e984303ed2
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2026-02-21 23:16:41 +0600

    add js connect external commit

---
+++ projukti-plus/1.5 js-connect/main.js
@@ -0,0 +1,3 @@
+console.log("Test JavaScript Connect with HTML.");
+document.getElementById("paragraph-text").innerHTML = "This passage messag from external JavaScript file.";
+alert("Test external JavaScript connection.");
