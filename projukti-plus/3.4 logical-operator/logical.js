a91672c14a3d728d1b6d33ba54b1b7b8af7adbe4
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2026-03-05 18:07:47 +0600

    js logical operator and , or , not practice

---
+++ projukti-plus/3.4 logical-operator/logical.js
@@ -0,0 +1,43 @@
+/**
+ * Logical Operator
+ *
+ */
+// ! Logical &&
+
+console.log( true && true );
+console.log( true && false );
+console.log( false && true );
+console.log( false && false );
+
+console.log(  0 && 0 );
+console.log( 0 && 1 );
+console.log( 1 && 0 );
+console.log( 1 && 1 );
+
+//! Logical OR( || )
+
+console.log( true || true );
+console.log( true || false );
+console.log( false || true );
+console.log( false || false );
+
+console.log( 1 || 1 );
+console.log( 1 || 0 );
+console.log( 0 || 1 );
+console.log( 0 || 0 );
+
+//! Logical Not ( ! )
+
+console.log( ! true );
+console.log( ! false );
+
+console.log( 4 > 3 && 5 > 7 );
+console.log( 8 > 10 && 12 < 20 );
+console.log( 8 < 10 && 12 > 20 );
+console.log( 8 < 10 && 12 < 20 );
+
+
+console.log( 4 > 3 || 5 > 7 );
+console.log( 8 > 10 || 12 < 20 );
+console.log( 8 < 10 || 12 > 20 );
+console.log( 8 < 10 || 12 < 20 );
