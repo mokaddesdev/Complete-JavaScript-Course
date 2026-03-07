3c9b963851e3613f876684f7ea583f4a69045cb0
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2026-03-07 13:32:29 +0600

    js increment and decrement operator

---
+++ free-code-cump/script.js
@@ -0,0 +1,19 @@
+let age;
+console.log(age);
+// Good variable names
+let age1 = 25;
+console.log(age1); // 25
+
+let age2 = 25;
+console.log(age2); // 25
+age2 = 30;
+console.log(age2); // 30
+
+// Bad variable names
+let x = 10;
+let y = "John";
+
+let score = 10;
+console.log(score); // 10
+score = 20;
+console.log(score); // 20
---
+++ projukti-plus/3.5 inrement-decrement/operator.js
@@ -0,0 +1,38 @@
+/**
+ * Increment and Decrement operator
+ *  i++
+ *  i--
+ *  ++i
+ *  --i
+ */
+
+let i =4;
+console.log( i );
+
+i++
+console.log( i );
+
+i--
+console.log( i )
+
+i = 4;
+console.log( ++i );
+console.log( i++ );
+
+i =4;
+console.log( --i );
+console.log( i-- );
+
+let a = 5;
+let b = 6;
+let c = 0;
+c = a++;
+console.log( c );
+c = ++b
+console.log( c );
+
+c = --a;
+console.log( c );
+
+c = a--;
+console.log( c );
