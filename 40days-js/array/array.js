de537f6232097bf6de6ae689af731eee6b1d4b3b
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2026-04-03 22:33:26 +0600

    array introduction javascript

---
+++ 40days-js/array/array.js
@@ -0,0 +1,23 @@
+// console.log( 'clear load' );
+/**
+ * JavaScript Array Crash Course
+ * 
+ * - [] introduction
+ * - [] What is an array in javascript?
+ * - [] How to create an array in javascrit?
+ * - [] How to get element from an array in JS?
+ * - [] How to add elememnts to an array in JS?
+ * - [] How to remove elements from an array in JS?
+ * - [] How to copy and clone an array in JS?
+ * - [] How to deteremine if a value is a an array in JS?
+ * - [] Array destructuring in JS?
+ * - [] How to assign a default value to a variable?
+ * - [] How to skip a value in an array?
+ * - [] Netsed array destructuring in JS
+ * - [] How to use the rest parameter in js?
+ * - [] How to use the spread operator in JS?
+ * - [] Destructuring use cases in JS?
+ * - [] How to swap value with destructuring?
+ * - [] How to merge arrays?
+ * 
+ */
